f2255db7ab6e53981e80814c3aa9f2d4e2ce8db5
Mouse <mouse0w0@qq.com>
2026-02-26 01:44:49 +0800

    Update ListAddBenchmark

--- src/main/java/benchmark/ListAddBenchmark.java
+++ src/main/java/benchmark/ListAddBenchmark.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 # JMH version: 1.37
 # VM version: JDK 21.0.7, OpenJDK 64-Bit Server VM, 21.0.7+6-LTS
 # VM invoker: C:\Program Files\Zulu\zulu-21\bin\java.exe
-# VM options: -javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2025.1.1.1\lib\idea_rt.jar=37789 -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8
+# VM options: -javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2025.1.1.1\lib\idea_rt.jar=40236 -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8
 # Blackhole mode: compiler (auto-detected, use -Djmh.blackhole.autoDetect=false to disable)
 # Warmup: 2 iterations, 10 s each
 # Measurement: 2 iterations, 10 s each
@@ -19,16 +19,16 @@ import java.util.concurrent.TimeUnit;
 # Benchmark mode: Average time, time/op
 
 Benchmark                 (count)  Mode  Cnt     Score   Error  Units
-ListAddBenchmark.add            0  avgt    2     6.264          ns/op
-ListAddBenchmark.add            1  avgt    2    11.067          ns/op
-ListAddBenchmark.add           10  avgt    2    25.978          ns/op
-ListAddBenchmark.add          100  avgt    2   263.548          ns/op
-ListAddBenchmark.add         1000  avgt    2  2149.510          ns/op
-ListAddBenchmark.lazyAdd        0  avgt    2     0.389          ns/op
-ListAddBenchmark.lazyAdd        1  avgt    2     9.578          ns/op
-ListAddBenchmark.lazyAdd       10  avgt    2    62.864          ns/op
-ListAddBenchmark.lazyAdd      100  avgt    2   675.881          ns/op
-ListAddBenchmark.lazyAdd     1000  avgt    2  6942.402          ns/op
+ListAddBenchmark.add            0  avgt    2     6.085          ns/op
+ListAddBenchmark.add            1  avgt    2    10.787          ns/op
+ListAddBenchmark.add           10  avgt    2    24.185          ns/op
+ListAddBenchmark.add          100  avgt    2   264.447          ns/op
+ListAddBenchmark.add         1000  avgt    2  2149.941          ns/op
+ListAddBenchmark.lazyAdd        0  avgt    2     0.361          ns/op
+ListAddBenchmark.lazyAdd        1  avgt    2     9.391          ns/op
+ListAddBenchmark.lazyAdd       10  avgt    2    61.358          ns/op
+ListAddBenchmark.lazyAdd      100  avgt    2   670.123          ns/op
+ListAddBenchmark.lazyAdd     1000  avgt    2  6778.650          ns/op
 */
 
 @Fork(1)
@@ -46,19 +46,21 @@ public class ListAddBenchmark {
     private List<Object> list;
 
     @Benchmark
-    public void add() {
+    public List<Object> add() {
         list = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             list.add(e);
         }
+        return list;
     }
 
     @Benchmark
-    public void lazyAdd() {
+    public List<Object> lazyAdd() {
         list = null;
         for (int i = 0; i < count; i++) {
             list = add(list, e);
         }
+        return list;
     }
 
     public static <E> List<E> add(List<E> list, E e) {
